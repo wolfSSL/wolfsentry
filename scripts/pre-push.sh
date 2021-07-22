54aabf16949ce087cc54bd8d1a41f47804ce6252
Daniel Pouzzner <douzzer@wolfssl.com>
2021-07-22 17:58:45 -0500

    scripts/pre-push.sh: preserve failure codes on exit.

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -11,16 +11,16 @@ trap "rm -rf $WORKDIR" EXIT
 remote="$1"
 url="$2"
 
-REPO_ROOT="$(git rev-parse --show-toplevel)" || exit 1
+REPO_ROOT="$(git rev-parse --show-toplevel)" || exit $?
 
-git clone -q --shared -n "$REPO_ROOT" "$WORKDIR" || exit 1
-cd "$WORKDIR" || exit 1
+git clone -q --shared -n "$REPO_ROOT" "$WORKDIR" || exit $?
+cd "$WORKDIR" || exit $?
 
 while read local_ref local_oid remote_ref remote_oid
 do
-	git checkout -q "$local_oid" || exit 1
+	git checkout -q "$local_oid" || exit $?
 	echo "make --quiet -j check for ${local_ref} at ${local_oid} ..."
-	make --quiet -j check || exit 1
+	make --quiet -j check || exit $?
 done
 
 exit 0

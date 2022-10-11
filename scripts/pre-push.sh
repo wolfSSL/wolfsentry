cf5c94dc7592a92a937c8d3d9052b96e1cc045d5
Daniel Pouzzner <douzzer@wolfssl.com>
2022-10-11 12:17:38 -0500

    scripts/pre-push.sh: "make check" only on Linux, otherwise "make test".

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+# shellcheck disable=SC2034 # allow unused variables
+
 if [ "$TMPDIR" = "" ]; then
     WORKDIR=/tmp/wolfsentry_for_push_hook.$$
 else
     WORKDIR="${TMPDIR}/wolfsentry_for_push_hook.$$"
 fi
 
-trap "rm -rf $WORKDIR" EXIT
+trap 'rm -rf "$WORKDIR"' EXIT
 
 remote="$1"
 url="$2"
@@ -16,14 +18,25 @@ REPO_ROOT="$(git rev-parse --show-toplevel)" || exit $?
 git clone -q --shared -n "$REPO_ROOT" "$WORKDIR" || exit $?
 cd "$WORKDIR" || exit $?
 
-while read local_ref local_oid remote_ref remote_oid
+if [ "$(uname -s)" = 'Linux' ]; then
+    have_linux=y
+else
+    have_linux=n
+fi
+
+while read -r local_ref local_oid remote_ref remote_oid
 do
     if [ "$local_ref" = "(delete)" ]; then
 	continue
     fi
     git checkout -q "$local_oid" || exit $?
-    echo "make --quiet -j check for ${local_ref} at ${local_oid} ..."
-    make --quiet -j check || exit $?
+    if [ "$have_linux" = 'y' ]; then
+	echo "make --quiet -j check for ${local_ref} at ${local_oid} ..."
+	make --quiet -j check || exit $?
+    else
+	echo "make --quiet -j test for ${local_ref} at ${local_oid} ..."
+	make --quiet -j test || exit $?
+    fi
 done
 
 exit 0
